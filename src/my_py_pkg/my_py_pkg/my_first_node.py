c5cbe56e8d249499bad14d0219915bf6dee06c7e
prateekbhaisora <prateek.bhaisora07@gmail.com>
2023-12-07 20:56:05 +0530

    Created a Python Publisher

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -7,7 +7,7 @@ class MyNode(Node):             # Creates a class inherit from Node object
     def __init__(self):                      # Creates a constructor
         super().__init__("py_test")          # Calls super method on node named py_test
         self.counter = 0                     # Added a counter
-        self.get_logger().info("Hello ROS2") # Print logs
+        self.get_logger().info("Hello ROS2!") # Print logs
         self.create_timer(0.5, self.timer_callback) # Uses create_timer method in Node object to call timer_callback function and print Hello every 2 Hz = 0.5 seconds
         
     def timer_callback(self):
