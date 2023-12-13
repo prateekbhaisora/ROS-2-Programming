b0ec65c9ad1126162e3f875752d15ea4928311c5
prateekbhaisora <prateek.bhaisora07@gmail.com>
2023-12-13 22:19:04 +0530

    Completed Activity 2

--- build/my_cpp_pkg/CMakeFiles/Makefile.cmake
+++ build/my_cpp_pkg/CMakeFiles/Makefile.cmake
@@ -698,4 +698,5 @@ set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/cpp_node.dir/DependInfo.cmake"
   "CMakeFiles/robot_news_station.dir/DependInfo.cmake"
   "CMakeFiles/smartphone.dir/DependInfo.cmake"
+  "CMakeFiles/number_counter.dir/DependInfo.cmake"
   )
