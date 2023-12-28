55df7610dbb85a8cdc5bcc3e58ea09fa8a0100fd
prateekbhaisora <prateek.bhaisora07@gmail.com>
2023-12-28 14:17:36 +0530

    Configured using Launch Files

--- build/my_py_pkg/build/lib/my_py_pkg/number_publisher.py
+++ build/my_py_pkg/build/lib/my_py_pkg/number_publisher.py
@@ -6,10 +6,15 @@ from example_interfaces.msg import Int64
 class NumberPublisherNode(Node): 
     def __init__(self):
         super().__init__("number_publisher") 
-        self.declare_parameter("test123")
-        self.number_ = 777
+        # self.declare_parameter("param_1_")
+        # self.declare_parameter("param_2_")
+        self.declare_parameter("number_to_publish", 2)
+        self.declare_parameter("publish_frequency", 1.0)
+        # Creates a parameter called number_to_pubish for number_publisher node with default value = 2
+        self.number_ = self.get_parameter("number_to_publish").value
+        self.publish_frequency_ = self.get_parameter("publish_frequency").value
         self.number_publisher_ = self.create_publisher(Int64, "number", 10)
-        self.timer_ = self.create_timer(1.0, self.publish_number_)
+        self.timer_ = self.create_timer(1.0 / self.publish_frequency_ , self.publish_number_)
         self.get_logger().info("Number Publisher has started publishing numbers.")
         
     def publish_number_(self):      
